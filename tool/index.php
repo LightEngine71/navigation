114907f5ef7b24dd5620b2159b75a6b46138e78e
jianghaosen <jianghaosen@hotmail.com>
2014-08-29 16:26:28 +0800

    new
    new

---
+++ tool/index.php
@@ -0,0 +1,197 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>工具栏</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+
+    <meta name="viewport" content="width=device-width">
+
+    
+  </head>
+  <body data-spy="scroll"  data-target=".navbar-example">
+    <div class="banner navbar-example ">
+      <ul>
+        <li style="background-image: url('images/sunset.jpg');">
+          <div class="inner">
+            <h1>The jQuery slider that just slides.</h1>
+            <p>就是这个不到3kb的插件！没有奇特的特效或无用的标签。</p>
+
+            <a class="btn" href="#download">下载</a>
+          </div>
+        </li>
+
+        <li style="background-image: url('images/shop.jpg');">
+          <div class="inner">
+            <h1>Fluid, flexible, fantastically minimal.</h1>
+            <p>Use any HTML in your slides, extend with CSS. You have full control.</p>
+
+            <a class="btn" href="#download">下载</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="pin">
+    <div class="features pinned ">
+      <div class="wrap cen ">
+          <b>工具类型</b>
+          <p><button type="button"  class="btn btn-primary btn-xs" href="#commonly"><a href="#commonly">常用站点</a></button>
+            <button type="button"  class="btn btn-success btn-xs"><a href="#group">团购检索端</a></button>
+            <button type="button" class="btn btn-info btn-xs"><a href="#pc">B端+PC</a></button>
+            <button type="button" class="btn btn-warning  btn-xs"><a href="#technology">技术专题</a></button>
+            <button type="button"  class="btn btn-danger btn-xs"><a href="#novice">新手入门</button></a></p>
+        
+
+      </div>
+    </div>
+    
+    <div id="commonly" class="how wrap">
+      <h2>
+        <span data-scroll-reveal="enter from the top over 1.5s" >常用站点</span>
+      </h2>
+      <div class="column">
+          <span class="block block-1x block-slate" data-scroll-reveal="enter top over 1s after 0.5s">机器管理平台</span>
+          <span class="block block-2x block-mango" data-scroll-reveal="enter right after 0.5s">团购组</span>
+          <span class="block block-1x block-kiwi" data-scroll-reveal="enter bottom over 1s and move 300px after 0.3s">QA-XPOWER组例会</span>
+          <span class="block block-3x block-blueberry" data-scroll-reveal="enter top over 0.5s and move 200px">dMapTopic</span>
+          <span class="block block-2x block-raspberry" data-scroll-reveal="enter bottom over 1s and move 100px">大家的足迹</span>
+      </div>
+      <div class="column">
+        <span class="block block-1x block-grape" data-scroll-reveal="enter top">搜索</span>
+        <span class="block block-1x block-orange" data-scroll-reveal="enter bottom over 1s and move 100px">河图</span>
+        <span class="block block-1x block-blueberry" data-scroll-reveal="enter top">任务管理平台</span>
+        <span class="block block-1x block-slate" data-scroll-reveal="enter right">环境部署平台</span>
+        <span class="block block-1x block-raspberry" data-scroll-reveal="enter bottom over 0.3s and move 50px">问卷检查平台</span>
+        <span class="block block-1x block-grape" data-scroll-reveal="enter left over 1s and move 300px after 0.3s">QA-XPower分享平台</span>
+      </div>
+      <div class="column">
+        <span class="block block-1x block-mango" data-scroll-reveal="enter bottom over 1s and move 300px after 0.3s">团购QA例会纪要</span>
+      </div>
+    </div>
+    <div id="group" class="how wrap">
+      <h2> 
+        <span data-scroll-reveal="enter from the top over 1.5s">团购检索端</span>
+      </h2>
+      <div class="column">
+        <span class="block block-1x block-slate" data-scroll-reveal="enter top over 1s after 0.5s">团购导航</span>
+        <span class="block block-2x block-mango" data-scroll-reveal="enter right after 0.5s">检索端工具</span>
+        <span class="block block-1x block-kiwi" data-scroll-reveal="enter bottom over 1s and move 300px after 0.3s">线上问题统计</span>
+        <span class="block block-3x block-blueberry" data-scroll-reveal="enter top over 0.5s and move 200px">QCASE</span>
+        <span class="block block-2x block-raspberry" data-scroll-reveal="enter bottom over 1s and move 100px">团购文档</span>
+      </div>
+       <div class="column">
+        <span class="block block-1x block-grape" data-scroll-reveal="enter top">测试问题</span>
+        <span class="block block-1x block-orange" data-scroll-reveal="enter bottom over 1s and move 100px">增量代码覆盖</span>
+        <span class="block block-1x block-blueberry" data-scroll-reveal="enter top">团购</span>
+        <span class="block block-1x block-slate" data-scroll-reveal="enter right">技术topic</span>
+        <span class="block block-1x block-raspberry" data-scroll-reveal="enter bottom over 0.3s and move 50px">监控</span>
+        <span class="block block-1x block-grape" data-scroll-reveal="enter left over 1s and move 300px after 0.3s">挖掘</span>
+      </div>
+      <div class="column">
+        <span class="block block-1x block-slate" data-scroll-reveal="enter top over 1s after 0.5s">会议记录</span>
+        <span class="block block-2x block-mango" data-scroll-reveal="enter right after 0.5s">自动化</span>
+        <span class="block block-1x block-kiwi" data-scroll-reveal="enter bottom over 1s and move 300px after 0.3s">检索</span>
+       
+      </div>
+    </div>
+    <div id="pc" class="how wrap">
+      <h2> 
+        <span data-scroll-reveal="enter from the top over 1.5s">百度B端+PC</span>
+      </h2>
+      <div class="column">
+        <span class="block block-1x block-slate" data-scroll-reveal="enter top over 1s after 0.5s">机器管理平台</span>
+        <span class="block block-2x block-mango" data-scroll-reveal="enter right after 0.5s">团购组</span>
+        <span class="block block-1x block-kiwi" data-scroll-reveal="enter bottom over 1s and move 300px after 0.3s">QA-XPOWER组例会</span>
+        <span class="block block-3x block-blueberry" data-scroll-reveal="enter top over 0.5s and move 200px">dMapTopic</span>
+        <span class="block block-2x block-raspberry" data-scroll-reveal="enter bottom over 1s and move 100px">大家的足迹</span>
+      </div>
+    </div>
+    <div id="technology" class="how wrap">
+      <h2> 
+        <span data-scroll-reveal="enter from the top over 1.5s">技术专题</span>
+      </h2>
+      <div class="column">
+        <span class="block block-1x block-slate" data-scroll-reveal="enter top over 1s after 0.5s">机器管理平台</span>
+        <span class="block block-2x block-mango" data-scroll-reveal="enter right after 0.5s">团购组</span>
+        <span class="block block-1x block-kiwi" data-scroll-reveal="enter bottom over 1s and move 300px after 0.3s">QA-XPOWER组例会</span>
+        <span class="block block-3x block-blueberry" data-scroll-reveal="enter top over 0.5s and move 200px">dMapTopic</span>
+        <span class="block block-2x block-raspberry" data-scroll-reveal="enter bottom over 1s and move 100px">大家的足迹</span>
+      </div>
+    </div>
+    <div id="novice" class="how wrap">
+      <h2> 
+        <span data-scroll-reveal="enter from the top over 1.5s">新手入门</span>
+      </h2>
+      <div class="column">
+        <span class="block block-1x block-slate" data-scroll-reveal="enter top over 1s after 0.5s">机器管理平台</span>
+        <span class="block block-2x block-mango" data-scroll-reveal="enter right after 0.5s">团购组</span>
+        <span class="block block-1x block-kiwi" data-scroll-reveal="enter bottom over 1s and move 300px after 0.3s">QA-XPOWER组例会</span>
+        <span class="block block-3x block-blueberry" data-scroll-reveal="enter top over 0.5s and move 200px">dMapTopic</span>
+        <span class="block block-2x block-raspberry" data-scroll-reveal="enter bottom over 1s and move 100px">大家的足迹</span>
+      </div>
+    </div>
+    </div> 
+
+   
+  </body>
+</html>
+<script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
+<script src="js/unslider.min.js"></script>
+<script>
+  if(window.chrome) {
+    $('.banner li').css('background-size', '100% 100%');
+  }
+
+  $('.banner').unslider({
+    arrows: true,
+    fluid: true,
+    dots: true
+  });
+
+  //  Find any element starting with a # in the URL
+  //  And listen to any click events it fires
+  $('a[href^="#"]').click(function() {
+    //  Find the target element
+    var target = $($(this).attr('href'));
+
+    //  And get its position
+    var pos = target.offset(); // fallback to scrolling to top || {left: 0, top: 0};
+
+    //  jQuery will return false if there's no element
+    //  and your code will throw errors if it tries to do .offset().left;
+    if(pos) {
+      //  Scroll the page
+      $('html, body').animate({
+        scrollTop: pos.top,
+        scrollLeft: pos.left
+      }, 1000);
+    }
+
+    //  Don't let them visit the url, we'll scroll you there
+    return false;
+  });
+
+  
+</script>
+
+<script src="js/scrollReveal.js"></script>
+<script src="js/jquery.pin.js"></script>
+<script type="text/javascript">
+    $(".pinned").pin({containerSelector: ".pin", minWidth: 940});
+</script>
+<script type="text/javascript">
+
+  (function($) {
+
+    'use strict';
+
+    window.scrollReveal = new scrollReveal({ reset: true, move: '50px' });
+  })();
+ //  $('body').scrollspy({ target: '.navbar-example' })
+   
+</script>
+
